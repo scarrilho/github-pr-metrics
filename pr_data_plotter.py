7643e9580bccc69ff84db025ff253247d19a0d42
Sergio <5166193+scarrilho@users.noreply.github.com>
2023-01-04 01:18:01 +0900

    Add base scripts
    Add retriever and plotter scripts

---
+++ pr_data_plotter.py
@@ -0,0 +1,303 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Change this setting as needed
+file_for_analysis = 'pr_data_github-pr-metrics_2023-01-04.csv'
+
+
+class PlotterHelper:
+    def plot_single(self, x, y, x_label, y_label, title, annotation_text, file_name):
+        print('Plotting')
+        text_y_position = y.max() - (y.mean()/3)
+
+        plt.close('all')
+        plt.bar(x, y, color='g')
+        plt.xticks(range(len(x)), x, size='small')
+        plt.locator_params(nbins=10)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.text(0, text_y_position, annotation_text, fontsize=13,
+                 bbox=dict(facecolor='red', alpha=0.5))
+        plt.title(title, fontsize=20)
+        plt.savefig(file_name)
+        plt.show()
+
+    def plot_double(self, x, y1, y2, y1_legend, y2_legend, x_label, y_label, title, file_name):
+        print('Plotting')
+        plt.close('all')
+        plt.plot(x, y1, color='g', label=y1_legend)
+        plt.plot(x, y2, color='r', label=y2_legend)
+
+        plt.xticks(range(len(x)), x, size='small')
+        plt.locator_params(nbins=10)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.title(title, fontsize=20)
+        plt.legend(bbox_to_anchor=(1, 1), loc='upper right',
+                   borderaxespad=1, fontsize=10)
+        plt.savefig(file_name)
+
+        plt.show()
+
+    def plot_double_filled(self, x, y1, y2, y1_legend, y2_legend, x_label, y_label, title, file_name):
+        print('Plotting')
+        plt.close('all')
+        plt.plot(x, y1, color='g', label=y1_legend)
+        plt.fill_between(x, y1, color='g', alpha=0.3)
+
+        plt.plot(x, y2, color='r', label=y2_legend)
+        plt.fill_between(x, y2, color='r', alpha=0.3)
+
+        plt.xticks(range(len(x)), x, size='small')
+        plt.locator_params(nbins=10)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.title(title, fontsize=20)
+        plt.legend(bbox_to_anchor=(1, 1), loc='upper right',
+                   borderaxespad=1, fontsize=10)
+        plt.savefig(file_name)
+
+        plt.show()
+
+    def plot_triple_filled(self, x, y1, y2, y3, y1_legend, y2_legend, y3_legend, x_label, y_label, title, annotation_text, file_name):
+        print('Plotting')
+        plt.close('all')
+        plt.plot(x, y1, color='g', label=y1_legend)
+
+        plt.plot(x, y2, color='r', label=y2_legend)
+        plt.fill_between(x, y1, y2, color='g', alpha=0.3)
+        plt.fill_between(x, 0, y2, color='r', alpha=0.4)
+
+        plt.plot(x, y3, color='b', label=y3_legend)
+
+        text_y_position = y3.max() - y1.mean()
+
+        plt.xticks(range(len(x)), x, size='small')
+        plt.locator_params(nbins=10)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.title(title, fontsize=20)
+        plt.legend(bbox_to_anchor=(1, 1), loc='upper right',
+                   borderaxespad=1, fontsize=10)
+        plt.text(0, text_y_position, annotation_text, fontsize=13,
+                 bbox=dict(facecolor='red', alpha=0.5))
+        plt.savefig(file_name)
+
+        plt.show()
+
+    def plot_scatter(self, x, y, x_label, y_label, title, file_name):
+        print('Plotting')
+        plt.close('all')
+        plt.scatter(x, y, color='g', s=100)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.title(title, fontsize=20)
+        plt.savefig(file_name)
+        plt.show()
+
+    def plot_double_scatter(self, x, y1, y2, y1_legend, y2_legend, x_label, y_label, title, file_name):
+        print('Plotting')
+        plt.close('all')
+        plt.scatter(x, y1, color='g', s=100, label=y1_legend)
+        plt.scatter(x, y2, color='r', s=100, label=y2_legend)
+
+        plt.xticks(range(len(x)), x, size='small')
+        plt.locator_params(nbins=10)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.title(title, fontsize=20)
+        plt.legend(bbox_to_anchor=(1, 1), loc='upper right',
+                   borderaxespad=1, fontsize=10)
+        plt.savefig(file_name)
+        plt.show()
+
+    # Number of new PRs daily
+    def plot_pr_count_vs_date(self, df):
+        filename = 'count_vs_date.png'
+        new_df = df['Creation Date'].value_counts().rename_axis(
+            'Date').reset_index(name='counts').sort_values('Date')
+        mean = round(new_df['counts'].mean(), 1)
+        annotation_text = 'Mean: ' + str(mean)
+        self.plot_single(
+            new_df['Date'], new_df['counts'], 'Date', 'PR count', 'Daily Created PR Count', annotation_text, filename)
+
+    def plot_pr_vs_review_comments(self, df):
+        filename = 'pr_vs_review_comments.png'
+        new_df = df[['PR Num', 'Review Comments']]
+        mean = round(new_df['Review Comments'].mean())
+        annotation_text = 'Mean: ' + str(mean)
+        self.plot_single(
+            new_df['PR Num'], new_df['Review Comments'], 'PR', 'Review Comments', 'Review Comment Count per PR', annotation_text, filename)
+
+    def plot_pr_size(self, df):
+        filename = 'pr_size.png'
+        new_df = df[['PR Num', 'PR Size']]
+        mean = round(new_df['PR Size'].mean())
+        annotation_text = 'Mean: ' + str(mean)
+        self.plot_single(
+            new_df['PR Num'], new_df['PR Size'], 'PRs', 'PR Size', 'PR Sizes', annotation_text, filename)
+
+    # Low lead time == good
+    def plot_lead_time_vs_additions_scatter(self, df):
+        filename = 'lead_time_vs_additions.png'
+        self.plot_scatter(
+            df['Lead Time'], df['Additions'], 'Lead Time (days)', 'Additions', 'PR lead time vs additions', filename)
+
+    # Low lead time == good
+    def plot_lead_time_vs_pr_size(self, df):
+        filename = 'lead_time_vs_pr_size.png'
+        self.plot_scatter(
+            df['Lead Time'], df['PR Size'], 'Lead Time (days)', 'PR Size (Additions + Deletions)', 'PR Lead Time vs PR Size', filename)
+
+    # To check balance of created vs closed
+    def plot_created_closed_daily(self, df):
+        filename = 'create_vs_closed_daily.png'
+        new_df = self.compute_daily_created_closed_table(df)
+        self.plot_double(
+            new_df['Date'], new_df['created'], new_df['closed'], 'Created', 'Closed', 'Date', 'Created/Closed', 'Created vs closed', filename)
+
+    # To check balance of created vs merged
+    def plot_created_merged_daily(self, df):
+        filename = 'created_vs_merged_daily.png'
+        new_df = self.compute_daily_created_merged_table(df)
+        self.plot_double(
+            new_df['Date'], new_df['created'], new_df['merged'], 'Created', 'Merged', 'Date', 'Created/Merged', 'Created vs Merged', filename)
+
+    # Check if keeping the code healthy by refactoring or just adding code
+    def plot_additions_deletions_scatter(self, df):
+        filename = 'additions_vs_deletions.png'
+        self.plot_double_scatter(
+            df['Merge Date'], df['Additions'], -df['Deletions'], 'Additions', 'Deletions', 'Merge Date', 'Additions', 'Additions vs Deletions', filename)
+
+    # Check if keeping the code healthy by refactoring or just adding code
+    def plot_additions_deletions_aggregate(self, df):
+        filename = 'additions_vs_deletions_aggregate.png'
+        new_df = self.compute_daily_added_deleted_table(df)
+        self.plot_double_filled(
+            new_df['Merge Date'], new_df['Additions'], new_df['Deletions'], 'Additions', 'Deletions', 'Merge Date', 'Additions/Deletions', 'Additions vs Deletions', filename)
+
+    # Check if keeping the code healthy by refactoring or just adding code
+    def plot_additions_deletions_pr_size_aggregate(self, df):
+        filename = 'additions_vs_deletions_pr_size_aggregate.png'
+        new_df = self.compute_daily_added_deleted_table(df)
+        total_modified = new_df['PR Size'].sum()
+        total_deleted = new_df['Deletions'].sum()
+        refactoring_ratio = round((total_deleted / total_modified), 2)
+        annotation_text = 'Refactor ratio: ' + str(refactoring_ratio)
+
+        self.plot_triple_filled(
+            new_df['Merge Date'], new_df['Additions'], new_df['Deletions'], new_df['PR Size'], 'Additions', 'Deletions', 'PR Size', 'Merge Date', 'Additions/Deletions', 'Additions vs Deletions vs PR Size', annotation_text, filename)
+
+    # Check if keeping the code healthy by refactoring or just adding code
+    def plot_additions_deletions_aggregate_inverse(self, df):
+        filename = 'additions_vs_deletions_aggregate_inverse.png'
+        new_df = self.compute_daily_added_deleted_table(df)
+        new_df['Deletions'] = -new_df['Deletions']
+
+        self.plot_double_filled(
+            new_df['Merge Date'], new_df['Additions'], new_df['Deletions'], 'Additions', 'Deletions', 'Merge Date', 'Additions/Deletions', 'Additions vs Deletions', filename)
+
+    def compute_daily_created_closed_table(self, df):
+        pr_creation_dates = df['Creation Date'].value_counts().rename_axis(
+            'Date').reset_index(name='created').sort_values('Date')
+
+        pr_closing_dates = df['Closing Date'].value_counts().rename_axis(
+            'Date').reset_index(name='closed').sort_values('Date')
+
+        new_df = pd.merge(pr_creation_dates,
+                          pr_closing_dates, how='outer', on='Date').sort_values('Date')
+
+        new_df = new_df.fillna(0)
+
+        return new_df
+
+    def compute_daily_created_merged_table(self, df):
+        pr_creation_dates = df['Creation Date'].value_counts().rename_axis(
+            'Date').reset_index(name='created').sort_values('Date')
+
+        pr_merging_dates = df['Merge Date'].value_counts().rename_axis(
+            'Date').reset_index(name='merged').sort_values('Date')
+
+        new_df = pd.merge(pr_creation_dates,
+                          pr_merging_dates, how='outer', on='Date').sort_values('Date')
+
+        new_df = new_df.fillna(0)
+
+        return new_df
+
+    def compute_daily_added_deleted_table(self, df):
+        aggregation_rule = {'Additions': 'sum',
+                            'Deletions': 'sum', 'PR Size': 'sum'}
+        df_aggregated = df.groupby('Merge Date', as_index=False).aggregate(
+            aggregation_rule).reindex(columns=df.columns)
+
+        return df_aggregated
+
+
+class StatsHelper:
+    def compute_main_stats(df):
+        sorted_by_merge_df = df.sort_values('Merge Date')
+
+        start_date = sorted_by_merge_df['Merge Date'].iloc[0]
+        end_date = sorted_by_merge_df['Merge Date'].iloc[-1]
+
+        print('---------------------------------------------------')
+        print('Main stats from {} to {}'.format(start_date, end_date))
+        print('---------------------------------------------------')
+
+        num_of_developers = len(pd.unique(df['User']))
+        num_of_prs = len(pd.unique(df['PR Num']))
+
+        df = df.loc[:, df.columns != 'PR Num']
+        df = df.loc[:, df.columns != 'Commits']
+        df = df.loc[:, df.columns != 'Comments']
+        metrics = df.describe().loc[['max', 'mean', 'min']]
+        metrics_formatted = metrics.applymap('{:,.0f}'.format)
+
+        print(
+            f'{num_of_prs} PRs, managed by {num_of_developers} developers were analyzed')
+
+        print(metrics_formatted)
+
+    def print_data_stats(df):
+        print(df.describe())
+
+    def abort_on_empty_df(df):
+        if df.empty == True:
+            print('---------------------(✘o✘)-------------------------')
+            print('Dataframe is empty, aborting execution!')
+            print('---------------------------------------------------')
+            exit(1)
+
+
+class Filter:
+    # Optional: Irregular PRs that can be kept out of the statistics
+    pull_requests_to_ignore = {}
+
+    def compute_filtered_list(self, df):
+        df_filtered = df[~df['PR Num'].isin(self.pull_requests_to_ignore)]
+        return df_filtered
+
+
+plotter = PlotterHelper()
+filter = Filter()
+
+df = pd.read_csv(file_for_analysis)
+df = filter.compute_filtered_list(df)
+
+StatsHelper.abort_on_empty_df(df)
+
+# Plot info
+print(f'Analyzing file: {file_for_analysis}')
+StatsHelper.compute_main_stats(df)
+
+plotter.plot_pr_count_vs_date(df)
+plotter.plot_lead_time_vs_pr_size(df)
+plotter.plot_created_merged_daily(df)
+plotter.plot_additions_deletions_pr_size_aggregate(df)
+plotter.plot_additions_deletions_aggregate_inverse(df)
+plotter.plot_pr_size(df)
+plotter.plot_pr_vs_review_comments(df)
+
+print('*************DONE*****************')
---
+++ pr_data_retriever.py
@@ -0,0 +1,175 @@
+import os
+import requests
+from datetime import datetime
+import pytz
+import csv
+
+# Set your repo info here.
+# Example: for https://github.com/scarrilho/github-pr-metrics
+OWNER = 'scarrilho'
+REPO_NAME = 'github-pr-metrics'
+USER_TO_IGNORE = 'Automated User'  # Optional
+
+# True for GitHub Enterprise repos, False for public repos
+# If True, in your shell, export you token: export GITHUB_API_TOKEN=your-token
+NEEDS_AUTHENTICATION = True
+
+# Adjust num of PRs to be retrieved
+MAX_NUM_PAGES = 5
+PULL_REQUESTS_PER_PAGE = 10
+
+current_page = 1
+
+# GitHub links
+LISTING_BASE_URL = f'https://api.github.com/repos/{OWNER}/{REPO_NAME}/pulls?state=closed&per_page={PULL_REQUESTS_PER_PAGE}&page='
+PR_BASE_URL = f'https://api.github.com/repos/{OWNER}/{REPO_NAME}/pulls/'
+PR_LINK_SUFFIX = f'https://github.com/{OWNER}/{REPO_NAME}/pull/'
+
+final_dataset = []
+date_format = '%Y-%m-%dT%H:%M:%SZ'
+
+
+class RequestHelper:
+    server_timezone = pytz.timezone('UTC')
+    tokyo_timezone = pytz.timezone('Asia/Tokyo')  # Using Tokyo time
+
+    def fetch_prs_listing_page(self, page):
+        url = LISTING_BASE_URL + str(page)
+        return self.execute_request(url)
+
+    def fetch_pr(self, pull_request_num):
+        url = PR_BASE_URL + str(pull_request_num)
+        print(url)
+
+        return self.execute_request(url)
+
+    def fetch_commits(self, pull_request_num):
+        url = PR_BASE_URL + str(pull_request_num) + '/commits'
+        return self.execute_request(url)
+
+    def fetch_first_commit_date(self, pull_request_num):
+        url = PR_BASE_URL + str(pull_request_num) + '/commits'
+        res = self.execute_request(url)
+        data_dict = res.json()[0]
+        date = data_dict.get('commit').get('author').get(
+            'date')
+        return date
+
+    def localize_datetime_to_tokyo(self, datetime):
+        datetime_in_tokyo = self.server_timezone.localize(
+            datetime).astimezone(self.tokyo_timezone)
+        return datetime_in_tokyo
+
+    def execute_request(self, url):
+        if NEEDS_AUTHENTICATION == False:
+            return requests.get(url)
+        else:
+            # In your shell, export you token: export GITHUB_API_TOKEN=your-token
+            github_token = os.environ['GITHUB_API_TOKEN']
+
+            # Or hardcode your token below
+            # github_token = 'your-token'
+
+            auth_info = 'Bearer ' + github_token
+            return requests.get(url, headers={
+                'Authorization': auth_info})
+
+
+class CSVWriter:
+    today = str(datetime.today().date())
+    target_csv_file_name = f'pr_data_{REPO_NAME}_{today}.csv'
+
+    def write_to_file(self, dataset):
+        print('-----------------')
+        print('Writing to csv, Please Wait')
+        keys = dataset[0].keys()
+        with open(self.target_csv_file_name, 'w', newline='') as output_file:
+            dict_writer = csv.DictWriter(output_file, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(dataset)
+
+
+request_helper = RequestHelper()
+csv_writer = CSVWriter()
+
+print(
+    f'Will start fetching {MAX_NUM_PAGES} pages, each with links to {PULL_REQUESTS_PER_PAGE} pull requests')
+
+# To add first data to res
+print(f'Fetching page {current_page} of {MAX_NUM_PAGES}')
+res = request_helper.fetch_prs_listing_page(current_page)
+print(res)
+
+repos = res.json()
+
+while res.json() != [] and current_page < MAX_NUM_PAGES:
+    current_page = current_page + 1
+
+    print(f'Fetching page {current_page} of {MAX_NUM_PAGES}')
+    res = request_helper.fetch_prs_listing_page(current_page)
+    print(res)
+    repos.extend(res.json())
+
+print('PR info retrieval complete. Will start fetching PRs')
+print('-----------------')
+print('Fetching PRs, Please Wait')
+for data in repos:
+    result = {}
+    pr_num = data['number']
+
+    # Ignore PRs that were not merged
+    if data['merged_at'] == None:
+        print('Ignoring unmerged PR: ', pr_num)
+        continue
+
+    first_commit = request_helper.fetch_first_commit_date(pr_num)
+
+    merged_at = datetime.strptime(data['merged_at'], date_format)
+    created_at = datetime.strptime(data['created_at'], date_format)
+    closed_at = datetime.strptime(data['closed_at'], date_format)
+    first_committed_at = datetime.strptime(
+        first_commit, date_format)
+
+    created_at_localized = request_helper.localize_datetime_to_tokyo(
+        created_at)
+    closed_at_localized = request_helper.localize_datetime_to_tokyo(closed_at)
+    merged_at_localized = request_helper.localize_datetime_to_tokyo(merged_at)
+    first_committed_at_localized = request_helper.localize_datetime_to_tokyo(
+        first_committed_at)
+
+    first_commit_date = first_committed_at.date()
+
+    result['Creation Date'] = created_at_localized.date()
+
+    result['Merge Date'] = merged_at_localized.date()
+
+    result['Closing Date'] = closed_at_localized.date()
+
+    result['First Commit Date'] = first_committed_at_localized.date()
+
+    result['Lead Time'] = round(
+        (merged_at_localized - created_at_localized).total_seconds() / 86400, 1)
+
+    result['Time to Merge'] = round(
+        (merged_at_localized - first_committed_at_localized).total_seconds() / 86400, 1)
+
+    res = request_helper.fetch_pr(pr_num)
+    result['Additions'] = res.json()['additions']
+    result['Deletions'] = res.json()['deletions']
+    result['PR Size'] = result['Additions'] + result['Deletions']
+    result['Changed Files'] = res.json()['changed_files']
+    result['Commits'] = res.json()['commits']
+    result['Review Comments'] = res.json()['review_comments']
+    result['Comments'] = res.json()['comments']
+    result['User'] = res.json()['user']['login']
+    result['PR Num'] = str(data['number'])
+    result['PR Link'] = PR_LINK_SUFFIX + str(data['number'])
+
+    if result['User'] != USER_TO_IGNORE:
+        final_dataset.append(result)
+
+if final_dataset != []:
+    csv_writer.write_to_file(final_dataset)
+    print('Success. Wrote to file: ', CSVWriter.target_csv_file_name)
+
+print('-------END-------')
